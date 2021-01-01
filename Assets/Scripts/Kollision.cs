02d5a2d43b93df9495086bf41efdef5ba0b140c1
Thomas Bertels <tbertels@web.de>
2021-01-01 15:12:47 +0100

    General - Preparing collision and rotation

---
+++ Assets/Scripts/Kollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("Hier ist was passiert");
+    }
+
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collisionInfo)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float x_rota = 0;
+    public float y_rota = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            x_rota = 20* Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            x_rota = -20* Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            y_rota = 20* Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            y_rota = -20* Time.deltaTime;
+        } else {
+            x_rota = 0;
+            y_rota = 0;
+        }
+        
+        // transform.rotation = transform.rotation += transform.Rotate(x_rota);
+        transform.Rotate(x_rota, y_rota, 0);
+    }
+}
