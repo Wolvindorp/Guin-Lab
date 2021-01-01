e46f92afbb94d7e3204d1438b3d35500a885dc39
Thomas Bertels <tbertels@web.de>
2021-01-01 15:08:53 +0100

    GL-3: some movements

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jump = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump")) {
+            jump = 20* Time.deltaTime;
+        } else {
+            jump = 0;
+        }
+        transform.Translate(0, jump, 0);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 15;
+    public float gravity;
+
+    private CharacterController characterController;
+    private float currentGravity = 0;
+
+
+    void Start() {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 finalMovement = MoveChar() + ApplyGravity();
+        if (!characterController) {
+            Debug.Log("Der characterController ist nicht gesetzt");
+        } else {
+            characterController.Move(finalMovement * Time.deltaTime);
+
+        }
+    }
+
+    Vector3 MoveChar() {
+        Vector3 moveVector = Vector3.zero;
+
+        moveVector += transform.forward * Input.GetAxis("Vertical");
+        moveVector += transform.right * Input.GetAxis("Horizontal");
+
+        moveVector *= moveSpeed;
+
+        return moveVector;
+    }
+
+    Vector3 ApplyGravity() {
+        Vector3 gravityMovement = new Vector3(0, -currentGravity, 0);
+        currentGravity += gravity * Time.deltaTime;
+
+        if ( characterController.isGrounded && currentGravity > 1f) {
+            currentGravity = 1f;
+        }
+
+        return gravityMovement;
+    }
+}
