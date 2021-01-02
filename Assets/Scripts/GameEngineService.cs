245141540a4080999f3cabfdf621f8432dc5207f
Thomas Bertels <tbertels@web.de>
2021-01-02 16:47:07 +0100

    GL-8: adjustments

---
+++ Assets/Scripts/GameEngineService.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEngineService : MonoBehaviour
+{
+    public string sceneName;
+    public void endGame() {
+        Debug.Log("GameOver");
+    }
+    public void startGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void quit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
