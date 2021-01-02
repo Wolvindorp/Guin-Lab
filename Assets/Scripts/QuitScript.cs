e483bdb9b7179d97387a8869c1e00f9f5839d847
Thomas Bertels <tbertels@web.de>
2021-01-02 14:23:22 +0100

    GL-8: Add game menu

---
+++ Assets/Scripts/QuitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    public void quit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
